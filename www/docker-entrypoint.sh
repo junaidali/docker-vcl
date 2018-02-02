3ce246029b9e7bf609e4ee1708abac8ebf7ae32f
Junaid ali <mailtojunaid@gmail.com>
2018-02-02 12:29:40 -0600

    website is now working fine as a kubernetes pod exposed using nodeport service

---
+++ www/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+echo "[Entrypoint] VCL Website"
+
+# Set secrets
+echo "[Entrypoint] Setting up secrets.php"
+cp /var/www/html/vcl/.ht-inc/secrets-default.php /var/www/html/vcl/.ht-inc/secrets.php
+sed -i "s/^\(\$vclhost\s=\s\).*/\1\'${MYSQL_HOST//\//\\/}';/" /var/www/html/vcl/.ht-inc/secrets.php
+sed -i "s/^\(\$vcldb\s=\s\).*/\1\'${MYSQL_DATABASE//\//\\/}';/" /var/www/html/vcl/.ht-inc/secrets.php
+sed -i "s/^\(\$vclusername\s=\s\).*/\1\'${MYSQL_USER//\//\\/}';/" /var/www/html/vcl/.ht-inc/secrets.php
+sed -i "s/^\(\$vclpassword\s=\s\).*/\1\'${MYSQL_PASSWORD//\//\\/}';/" /var/www/html/vcl/.ht-inc/secrets.php
+sed -i "s/^\(\$cryptkey\s=\s\).*/\1\'${VCL_CRYPT_KEY//\//\\/}';/" /var/www/html/vcl/.ht-inc/secrets.php
+sed -i "s/^\(\$pemkey\s=\s\).*/\1\'${VCL_PEM_KEY//\//\\/}';/" /var/www/html/vcl/.ht-inc/secrets.php
+echo "[Entrypoint] secrets.php updated"
+cat /var/www/html/vcl/.ht-inc/secrets.php
+
+# Run key generation
+echo "[Entrypoint] Generating encryption keys"
+cd /var/www/html/vcl/.ht-inc
+./genkeys.sh
+
+# Start HTTPD
+echo "[Entrypoint] Starting VCL Website using $@"
+exec "$@"
