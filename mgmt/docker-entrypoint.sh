894d0b605ea5b5e190e4b5c69fc6aacdffd2287e
Junaid Ali <alisyed8@iit.edu>
2018-07-23 12:17:06 -0500

    Added support for postfix mail transfer agent within the managementnode container.

--- mgmt/docker-entrypoint.sh
+++ mgmt/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 echo "[Entrypoint] VCL Management Daemon"
 
 # Update VCL Configuration File
+echo "[Entrypoint] Updating vcl daemon configuration file /etc/vcl/vcld.conf"
 sed -i "s/^\(FQDN=\).*/\1$HOSTNAME/" /etc/vcl/vcld.conf
 sed -i "s/^\(database=\).*/\1${MYSQL_DATABASE//\//\\/}/" /etc/vcl/vcld.conf
 sed -i "s/^\(server=\).*/\1${MYSQL_HOST//\//\\/}/" /etc/vcl/vcld.conf
@@ -14,12 +15,17 @@ sed -i "s/^\(xmlrpc_pass=\).*/\1${XMLRPC_PASSWORD//\//\\/}/" /etc/vcl/vcld.conf
 sed -i "s/^\(xmlrpc_username=\).*/\1${XMLRPC_USER//\//\\/}/" /etc/vcl/vcld.conf
 sed -i "s/^\(xmlrpc_url=\).*/\1${XMLRPC_URL//\//\\/}/" /etc/vcl/vcld.conf
 
-echo "[Entrypoint] Configuration File Updated"
+echo "[Entrypoint] VCL Configuration File Updated"
 cat /etc/vcl/vcld.conf
 
 # Update database
 echo "[Entrypoint] Updating management node information in database"
 perl -f /configure-vcl-db.pl
+
+# Configure Postfix MTA
+echo "[Entrypoint] Updating postfix configuration"
+postconf -e inet_interfaces=all
+postconf -e relayhost=${SMTP_RELAY_HOST}:${SMTP_RELAY_PORT}
 
 # Start VCL Daemon
 echo "[Entrypoint] Starting VCL Daemon using $@"
